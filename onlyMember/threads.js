d3f69fa537fe9a99243cf7d3f53fa2b6fee997ef
meganeGoods <meganegoods@gmail.com>
2025-10-19 00:35:55 +0900

    色々変更

--- onlyMember/threads.js
+++ onlyMember/threads.js
@@ -1,9 +1,9 @@
-function threadsComponent(data,index,isSecret){
+function threadsComponent(data,index,isSecret,isDisplay){
     const string = `
     <a 
         href="./thread/?page=${index}"
         class="thread_link" 
-        style="text-decoration:none; color:var(--text-dark);">
+        style="text-decoration:none; color:var(--text-dark); display:'${isDisplay ? "block" : "none"}'">
         
         <div class="thread ${isSecret ? "secret" : "public"}">
             <h1 class="thread_title">${data.title}</h1>
